3a938dd1b057ce0f8895cb358a96eaca1908f8b5
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-06-06 16:54:28 -0300

    Cronometrado Login e implementado backend.

---
+++ Backend/Services/Checkin/checkinService.php
@@ -0,0 +1,119 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+use Firebase\JWT\JWT;
+
+require_once __DIR__ . "/../../Connection/db.php";
+
+class CheckinService
+{
+    protected $db;
+
+
+    public function __construct()
+    {
+        $this->db = db();
+    }
+
+    public function buscarCheckinPorId($idCheckin)
+    {
+        if (empty($idCheckin)) {
+            throw new Exception('Dados inválidos', 400);
+        }
+
+        $buscar = $this->db->prepare('SELECT * FROM checkin WHERE id_checkin = :id_checkin');
+
+        $buscar->execute([
+            ':id_checkin' => $idCheckin
+        ]);
+
+        $checkin = $buscar->fetch();
+
+        if (empty($checkin)) {
+            return [
+                'sucesso' => false,
+                'mensagem' => "Checkin não encontrado",
+                'codigo' => 404
+            ];
+        }
+
+        return [
+            'sucesso' => true,
+            'dados' => $checkin
+        ];
+    }
+
+
+
+    public function listarCheckins()
+    {
+        $query = $this->db->query("SELECT * FROM checkin");
+
+        $query->execute();
+
+        $checkins = $query->fetchAll();
+
+        return [
+            'sucesso' => true,
+            'dados' => $checkins,
+            'total' => count($checkins)
+        ];
+    }
+
+    public function criarCheckin($checkinDados, $jwt)
+    {
+        try {
+
+            $buscarConvidado = $this->db->prepare('SELECT * FROM convidado WHERE id_convidado = :id_convidado');
+            $buscarConvidado->execute([
+                ':id_convidado' => $checkinDados['convidado_idconvidado']
+            ]);
+
+            $convidado = $buscarConvidado->fetch();
+
+            if (empty($convidado)) {
+                throw new Exception('Convidado não encontrado', 404);
+            }
+
+            if($convidado['confirmacao'] === 'confirmado'){
+                throw new Exception('Convidado já confirmado', 409);
+            }
+
+            $dataFormatada = date('Y-m-d H:i:s');
+
+            $criar = $this->db->prepare('INSERT INTO checkin (usuario_idusuario, convidado_idconvidado, data_e_hora)
+        VALUES (:usuario_idusuario, :convidado_idconvidado, :data_e_hora)');
+
+            $criar->execute([
+                ':usuario_idusuario' => $jwt->dados->id_usuario,
+                ':convidado_idconvidado' => $checkinDados['convidado_idconvidado'],
+                ':data_e_hora' => $dataFormatada
+            ]);
+
+            $atualizarConvidado = $this->db->prepare('UPDATE convidado SET confirmacao = "confirmado" WHERE id_convidado = :id_convidado');
+
+            $atualizarConvidado->execute([
+                ':id_convidado' => $checkinDados['convidado_idconvidado']
+            ]);
+
+            return [
+                'sucesso' => true,
+                'mensagem' => 'Checkin criado com sucesso'
+            ];
+        } catch (PDOException $e) {
+            if (str_contains($e->getMessage(), 'convidado_idconvidado')) {
+                throw new Exception('Convidado já confirmado', 409);
+            }
+
+            if (str_contains($e->getMessage(), 'fk_checkin_usuario')) {
+                throw new Exception('Usuário referenciado não encontrado', 409);
+            }
+
+            if (str_contains($e->getMessage(), 'fk_checkin_convidado')) {
+                throw new Exception('Convidado referenciado não encontrado', 404);
+            }
+
+            throw new Exception('Erro ao criar checkin', 500);
+        }
+    }
+}
--- Frontend/src/Pages/Login/login.jsx
+++ Frontend/src/Pages/Login/login.jsx
@@ -1,64 +1,114 @@
-import { useState } from "react";
-import fotoCasamento from "../../assets/Foto_casamento.jpg";
-import logoCasamento from "../../assets/Logo_casamento.jpg";
-
+import { Button, Container, Form, InputGroup, Stack } from "react-bootstrap";
+import imagem from "../../assets/Foto_casamento.jpg";
+import logo from "../../assets/Logo_casamento.jpg";
 import style from "./login.module.css";
-import { Button, Form, InputGroup, Stack } from "react-bootstrap";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+
+import { useState } from "react";
+import Api from "../../Service/api";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
 
-
 const Login = () => {
-    const [mostrarSenha, setMostrarSenha] = useState(false);
-    const navigate = useNavigate();
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [formData, setFormData] = useState({ email: "", senha: "" });
+const navigate = useNavigate();
+  const handleChange = (e) => {
+    const {name, value} = e.target  
+
+    if(!name) return
+
+    setFormData((prev) => ({...prev, [name]: value}))
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+
+        const res = await Api.post('/usuario/login', formData)
+
+        if(res.status === 200){
+            toast.success('Login realizado com sucesso')
+            localStorage.setItem('token', res.data.token)
+            navigate('/')
+        }
+    }catch(err){
+        const erros = err.response?.data?.erros;
+
+        if(erros){
+            Object.values(erros).forEach((msg) => {
+                toast.error(msg)
+            })
+        }else{
+            toast.error(err.response?.data?.mensagem || 'Erro ao enviar dados')
+        }
+    }
+  };
+
   return (
     <div className={style.loginContainer}>
-      <div className={style.fotoDiv}>
-        <img
-          className={style.foto}
-          src={fotoCasamento}
-          alt="Foto de casamento"
-        ></img>
+      <div className={style.divFoto}>
+        <img className={style.imagem} src={imagem} alt="Imagem casamento" />
       </div>
-      <Stack gap={2} className={style.formDiv}>
-        <img
-        className={style.Logo}
-        src={logoCasamento}
-        alt="Logo de casamento"
-        >
-        
-        </img>
+      <Stack gap={3} className={style.divForm}>
+        <img src={logo} className={style.logo} alt="Logo casamento" />
         <h1>Senac Wedding</h1>
         <h5>Seu portal de casamentos</h5>
-        <hr className=" w-75" />
-        <div className="w-75">
-          <Form className="d-flex flex-column gap-4">
+        <hr className="w-75" />
+        <Form onSubmit={handleSubmit} className="w-75">
+          <Stack gap={4}>
             <Form.Group>
-              <Form.Label>EMAIL</Form.Label>
-              <Form.Control
-              type="email"
-                className={style.input}
-                placeholder="ricardo@gmail.com"
-              />
+              <Form.Label>Email</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>
+                  <MdEmail />
+                </InputGroup.Text>
+                <Form.Control 
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Seu melhor email"
+                required
+                />
+              </InputGroup>
             </Form.Group>
             <Form.Group>
-              <Form.Label>SENHA</Form.Label>
+              <Form.Label>Senha</Form.Label>
               <InputGroup>
-                <Form.Control
-                  type={mostrarSenha ? "text" : "password"}
-                  placeholder="********"
-                  className={style.inputSenha}
+                <InputGroup.Text>
+                  <RiLockPasswordFill />
+                </InputGroup.Text>
+                <Form.Control 
+                type={mostrarSenha ? 'text' : 'password'}
+                value={formData.senha}
+                name="senha"
+                onChange={handleChange}
+                placeholder="Sua senha"
+                required
                 />
-                <Button onClick={() => setMostrarSenha(!mostrarSenha)} className={style.btnShowSenha}>
-                  {mostrarSenha ? <FaEye color="gray" size={25}/> : <FaEyeSlash color="gray" size={25}/>}
+                <Button
+                  onClick={() => setMostrarSenha(!mostrarSenha)}
+                  className="bg-transparent border "
+                >
+                  {mostrarSenha ? (
+                    <FaEye color="gray" />
+                  ) : (
+                    <FaEyeSlash color="gray" />
+                  )}
                 </Button>
               </InputGroup>
             </Form.Group>
-            <p className="text-muted">Esqueceu a senha?</p>
-            <Button onClick={() => navigate('/')} className={style.btnSubmit} type="button">LOGIN</Button>
-          </Form>
-        </div>
+          </Stack>
+          <hr className="mt-5" />
+          <Stack>
+            <Button className={style.btnSubmit} type="submit">
+              Login
+            </Button>
+          </Stack>
+        </Form>
       </Stack>
     </div>
   );
